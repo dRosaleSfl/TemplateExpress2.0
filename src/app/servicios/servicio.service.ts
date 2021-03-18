03a20c99ed80e5000fdf09bee153585678e67baa
hashanah <hashanahm@gmail.com>
2021-03-18 09:59:30 -0600

    proooooooooooooooooooooooxy

--- src/app/servicios/servicio.service.ts
+++ src/app/servicios/servicio.service.ts
@@ -17,10 +17,10 @@ export class ServicioService {
 
   //clientes
   getusuario(){
-    return this.httpClient.get('http://localhost:3000/clientes');
+    return this.httpClient.get('/basedatos-api/clientes');
   }
   getclient(id:string){
-    return this.httpClient.get(`http://localhost:3000/client?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/client?id=${id}`);
   }
   addClient(cliente:any){
     let nombre = cliente.nombre_cliente;
@@ -39,12 +39,12 @@ export class ServicioService {
     let ciudad =cliente.ciudad;
     let estado =cliente.estado;
     let pais =cliente.pais;
-    return this.httpClient.get(`http://localhost:3000/newclient?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
+    return this.httpClient.get(`/basedatos-api/newclient?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
   
    }
    deleteClient(cliente:any){
     let id = cliente.id_cliente;
-    return this.httpClient.get(`http://localhost:3000/deletclient?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/deletclient?id=${id}`);
    }
    ediclient(cliente:any){
     let id = cliente.id_cliente;
@@ -67,16 +67,16 @@ export class ServicioService {
     console.log(cliente);
     console.log(apepat);
   // return this.httpClient.get(`http://localhost:3000/updateclient?nombre=${nombre}&apepat=${apemat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}&id=${id}`);
-  return this.httpClient.get(`http://localhost:3000/updateclient?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
+  return this.httpClient.get(`/basedatos-api/updateclient?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
 
   }
 
 //----------------------------------empleados-------------------------------
   getempleados(){
-    return this.httpClient.get('http://localhost:3000/empleados');
+    return this.httpClient.get('/basedatos-api/empleados');
   }
   getempleado(id:string){
-    return this.httpClient.get(`http://localhost:3000/empleado?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/empleado?id=${id}`);
   }
   addempleado(empleado:any){
     let nombre = empleado.nombre_empleado;
@@ -95,12 +95,12 @@ export class ServicioService {
     let estado =empleado.estado;
     let pais =empleado.pais;
     console.log(nombre,apepat,apemat,telefono,calle,numint,numext,colonia,cp,ciudad,estado,pais,nombreu,contra,puesto);
-    return this.httpClient.get(`http://localhost:3000/newempleado?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&puesto=${puesto}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}&nombreu=${nombreu}&contra=${contra}`);
+    return this.httpClient.get(`/basedatos-api/newempleado?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&puesto=${puesto}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}&nombreu=${nombreu}&contra=${contra}`);
   
   }
   deletempleado(empleado:any){
     let id = empleado.id_empleado;
-    return this.httpClient.get(`http://localhost:3000/dempleado?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/dempleado?id=${id}`);
    }
 
    editempleado(empleado:any){
@@ -121,15 +121,15 @@ export class ServicioService {
     let estado =empleado.estado;
     let pais =empleado.pais;
     console.log(nombre,apepat,apemat,telefono,calle,numint,numext,colonia,cp,ciudad,estado,pais,nombreu,contra,puesto);
-    return this.httpClient.get(`http://localhost:3000/updateempleado?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&puesto=${puesto}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}&nombreu=${nombreu}&contra=${contra}`);
+    return this.httpClient.get(`/basedatos-api/updateempleado?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&puesto=${puesto}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}&nombreu=${nombreu}&contra=${contra}`);
   }
 
  ///--------------------------proveedores
   getproveedores(){
-  return this.httpClient.get('http://localhost:3000/proveedores');
+  return this.httpClient.get('/basedatos-api/proveedores');
   }
   getproveedor(id:string){
-   return this.httpClient.get(`http://localhost:3000/proveedor?id=${id}`);
+   return this.httpClient.get(`/basedatos-api/proveedor?id=${id}`);
   }
   addprovedor(proveedor:any){
     let nombre = proveedor.nombre_proveedor;
@@ -148,12 +148,12 @@ export class ServicioService {
     let ciudad =proveedor.ciudad;
     let estado =proveedor.estado;
     let pais =proveedor.pais;
-    return this.httpClient.get(`http://localhost:3000/newproveedor?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
+    return this.httpClient.get(`/basedatos-api/newproveedor?nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
   
   }
   deletproveedor(proveedor:any){
     let id = proveedor.id_proveedor;
-    return this.httpClient.get(`http://localhost:3000/dproveedor?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/dproveedor?id=${id}`);
    }
 
    editproveedor(proveedor:any){
@@ -175,20 +175,20 @@ export class ServicioService {
     let estado =proveedor.estado;
     let pais =proveedor.pais;
     console.log(nombre,apepat,apemat,telefono,calle,numint,numext,colonia,cp,ciudad,estado,pais,);
-    return this.httpClient.get(`http://localhost:3000/updateproveedor?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
+    return this.httpClient.get(`/basedatos-api/updateproveedor?id=${id}&nombre=${nombre}&apepat=${apepat}&apemat=${apemat}&telefono=${telefono}&rfc=${rfc}&razonsocial=${razonsocial}&tipo=${tipo}&correo=${correo}&calle=${calle}&numint=${numint}&numext=${numext}&colonia=${colonia}&cp=${cp}&ciudad=${ciudad}&estado=${estado}&pais=${pais}`);
   
   }
 //-------------------------------------------invetnario
   getinventario(){
-    return this.httpClient.get('http://localhost:3000/inventario');
+    return this.httpClient.get('/basedatos-api/inventario');
     }
     getproducto(id:string){
-     return this.httpClient.get(`http://localhost:3000/producto?id=${id}`);
+     return this.httpClient.get(`/basedatos-api/producto?id=${id}`);
     }
     deletproducto(producto:any){
       let id = producto.id_herraje;
       console.log(id);
-     return this.httpClient.get(`http://localhost:3000/dproducto?id=${id}`);
+     return this.httpClient.get(`/basedatos-api/dproducto?id=${id}`);
     }
     addproducto(producto:any){
       let id = producto.id_herraje;
@@ -199,7 +199,7 @@ export class ServicioService {
       let existencias = producto.existencias;
       let min = producto.min;
       console.log(producto);
-     return this.httpClient.get(`http://localhost:3000/newproducto?id=${id}&nombre=${nombre}&marca=${marca}&cvidrio=${cvidrio}&svidrio=${svidrio}&existencias=${existencias}&min=${min}`);
+     return this.httpClient.get(`/basedatos-api/newproducto?id=${id}&nombre=${nombre}&marca=${marca}&cvidrio=${cvidrio}&svidrio=${svidrio}&existencias=${existencias}&min=${min}`);
     }
     editproducto(producto:any){
       let id = producto.id_inventario;
@@ -211,11 +211,11 @@ export class ServicioService {
       let existencias = producto.existencias;
       let min = producto.min;
       console.log(producto);
-     return this.httpClient.get(`http://localhost:3000/upadateproducto?id=${id}&id_herraje=${id_herraje}&nombre=${nombre}&marca=${marca}&cvidrio=${cvidrio}&svidrio=${svidrio}&existencias=${existencias}&min=${min}`);
+     return this.httpClient.get(`/basedatos-api/upadateproducto?id=${id}&id_herraje=${id_herraje}&nombre=${nombre}&marca=${marca}&cvidrio=${cvidrio}&svidrio=${svidrio}&existencias=${existencias}&min=${min}`);
     }
 //--------------------precios-----
 getprecios(){
-  return this.httpClient.get('http://localhost:3000/precios');
+  return this.httpClient.get('/basedatos-api/precios');
   }
   getprecio(precio:any){
     let id = precio.id_listaprecios;
@@ -223,37 +223,37 @@ getprecios(){
      let id1=precio.id_proveedor;
      console.log(id);
      console.log(id1);
-   return this.httpClient.get(`http://localhost:3000/precio?id=${id}&id1=${id1}&id2=${id2}`);
+   return this.httpClient.get(`/basedatos-api/precio?id=${id}&id1=${id1}&id2=${id2}`);
   }
   deletprecio(producto:any){
     let id = producto.id_herraje;
     let id1 = producto.id_proveedor;
     console.log(id);
-   return this.httpClient.get(`http://localhost:3000/dprecio?id=${id}&id1=${id1}`);
+   return this.httpClient.get(`/basedatos-api/dprecio?id=${id}&id1=${id1}`);
   }
   addprecio(producto:any){
     let id = producto.id_herraje;
     let precio1 =producto.precio_compra;
     let id1 = producto.id_proveedor;
-   return this.httpClient.get(`http://localhost:3000/newwprecio?id=${id}&precio=${precio1}&id1=${id1}`);
+   return this.httpClient.get(`/basedatos-api/newwprecio?id=${id}&precio=${precio1}&id1=${id1}`);
   }
   updtaeprecio(producto:any){
     let id = producto.id_herraje;
     let id1 = producto.id_proveedor;
     let precio1 =producto.precio_compra;
            
-   return this.httpClient.get(`http://localhost:3000/upadateprecio?id=${id}&id1=${id1}&precio=${precio1}`);
+   return this.httpClient.get(`/basedatos-api/upadateprecio?id=${id}&id1=${id1}&precio=${precio1}`);
   }
   //-------------------Faltantes-----
   getfaltantes(){
-    return this.httpClient.get('http://localhost:3000/faltantes');
+    return this.httpClient.get('/basedatos-api/faltantes');
     }
 
   getfaltante(faltante:any){
       let id = faltante.id_faltantes;
       let id2=faltante.fecha;
        console.log(id);
-     return this.httpClient.get(`http://localhost:3000/faltante?id=${id}&id2=${id2}`);
+     return this.httpClient.get(`/basedatos-api/faltante?id=${id}&id2=${id2}`);
     }
 
   addFaltantes(faltante:any){
@@ -262,23 +262,23 @@ getprecios(){
       let cantidad = faltante.cantidad;
       let id1 = faltante.id_herraje;
       let id2 = faltante.id_proveedor;
-      return this.httpClient.get(`http://localhost:3000/newfaltantes?id=${id}&id1=${id1}&id2=${id2}&fecha=${fecha}&cantidad=${cantidad}`);
+      return this.httpClient.get(`/basedatos-api/newfaltantes?id=${id}&id1=${id1}&id2=${id2}&fecha=${fecha}&cantidad=${cantidad}`);
     
      }
 
     deletefaltante(faltante:any){
       let id = faltante.id_faltantes;
       console.log(id);
-     return this.httpClient.get(`http://localhost:3000/dfaltante?id=${id}`);
+     return this.httpClient.get(`/basedatos-api/dfaltante?id=${id}`);
     }
 
     deleteprod(faltante:any){
       let id1 = faltante.id_producto;
-     return this.httpClient.get(`http://localhost:3000/dprod?id1=${id1}`);
+     return this.httpClient.get(`/basedatos-api/dprod?id1=${id1}`);
     }
 //-------------------Ganancias-----
 getganancias(){
-  return this.httpClient.get('http://localhost:3000/ganancias');
+  return this.httpClient.get('/basedatos-api/ganancias');
   }
 
   addGanancias(ganancia:any){
@@ -291,11 +291,11 @@ getganancias(){
     let recibio = ganancia.recibio;
     let id1 = ganancia.id_cliente;
     let fecha = ganancia.fecha;
-    return this.httpClient.get(`http://localhost:3000/newganancias?id=${id}&num_nota=${num_nota}&tipo_pago=${tipo_pago}&cantidad=${cantidad}&concepto=${concepto}&status=${status}&recibio=${recibio}&fecha=${fecha}&id1=${id1}`);
+    return this.httpClient.get(`/basedatos-api/newganancias?id=${id}&num_nota=${num_nota}&tipo_pago=${tipo_pago}&cantidad=${cantidad}&concepto=${concepto}&status=${status}&recibio=${recibio}&fecha=${fecha}&id1=${id1}`);
    }
 
    getganancia(id:string){
-    return this.httpClient.get(`http://localhost:3000/ganancia?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/ganancia?id=${id}`);
    }
 
    editganancia(gan:any){
@@ -312,18 +312,18 @@ getganancias(){
     let recibio = gan.recibio;
     let fecha = gan.fecha;
     console.log(gan);
-   return this.httpClient.get(`http://localhost:3000/upadateganancia?id=${id}&num_nota=${num_nota}&nombre_cliente=${nombre_cliente}&ape_pat=${ape_pat}&ape_mat=${ape_mat}&id_cliente=${id_cliente}&tipo_pago=${tipo_pago}&cantidad=${cantidad}&concepto=${concepto}&status=${status}&recibio=${recibio}&fecha=${fecha}`);
+   return this.httpClient.get(`/basedatos-api/upadateganancia?id=${id}&num_nota=${num_nota}&nombre_cliente=${nombre_cliente}&ape_pat=${ape_pat}&ape_mat=${ape_mat}&id_cliente=${id_cliente}&tipo_pago=${tipo_pago}&cantidad=${cantidad}&concepto=${concepto}&status=${status}&recibio=${recibio}&fecha=${fecha}`);
   }
   
   deleteganancia(gan:any){
     let id = gan.id_ganancias;
     console.log(id);
-   return this.httpClient.get(`http://localhost:3000/dganancia?id=${id}`);
+   return this.httpClient.get(`/basedatos-api/dganancia?id=${id}`);
   }
 
   //Reporte diario
   gananciadiaria(fecha){
-    return this.httpClient.get(`http://localhost:3000/gananciasdiarias?id=${fecha}`)
+    return this.httpClient.get(`/basedatos-api/gananciasdiarias?id=${fecha}`)
   }
 
   ///----------------cambio de pantasha
@@ -342,34 +342,34 @@ getganancias(){
   }
   ////-----------------loooogin validar 
   validar(user:any){
-    return this.httpClient.get(`http://localhost:3000/user?username=${user.username}&contra=${user.contra}`);
+    return this.httpClient.get(`/basedatos-api/user?username=${user.username}&contra=${user.contra}`);
   }
 
   //Reportes
   getreporte() {
-    return this.httpClient.get('http://localhost:3000/getmensual');
+    return this.httpClient.get('/basedatos-api/getmensual');
   }
   getreportea() {
-    return this.httpClient.get('http://localhost:3000/getanual');
+    return this.httpClient.get('/basedatos-api/getanual');
   }
   getreportes(fechaaa) {
      let fecha=fechaaa;
     console.log("servicio");
     console.log(fecha);
     console.log(fechaaa+"servicio chido chido");
-    return this.httpClient.get(`http://localhost:3000/getsemanal?f_rep=${fecha}`);
+    return this.httpClient.get(`/basedatos-api/getsemanal?f_rep=${fecha}`);
   }
   mvendido() {
-    return this.httpClient.get('http://localhost:3000/masvendido');
+    return this.httpClient.get('/basedatos-api/masvendido');
   }
   mpedido() {
-    return this.httpClient.get('http://localhost:3000/maspedido');
+    return this.httpClient.get('/basedatos-api/maspedido');
   }
   mcaro() {
-    return this.httpClient.get('http://localhost:3000/mascaro');
+    return this.httpClient.get('/basedatos-api/mascaro');
   }
   clientemcompras(){
-    return this.httpClient.get('http://localhost:3000/clientemascompras');
+    return this.httpClient.get('/basedatos-api/clientemascompras');
   }
 
   //-----------Servicio añadir pedido----
@@ -378,42 +378,42 @@ getganancias(){
     let cantidad = pedido.cantidad;
     let precio_unitario = pedido.precio_unitario;
     let tipo_precio = pedido.tipo_precio;
-    return this.httpClient.get(`http://localhost:3000/newpedido?id_herraje=${id_herraje}&cantidad=${cantidad}&precio_unitario=${precio_unitario}&tipo_precio=${tipo_precio}`);
+    return this.httpClient.get(`/basedatos-api/newpedido?id_herraje=${id_herraje}&cantidad=${cantidad}&precio_unitario=${precio_unitario}&tipo_precio=${tipo_precio}`);
   }
   // Obtener el id del pedido
   getIdPedido() {
-    return this.httpClient.get(`http://localhost:3000/getIdPedido`);
+    return this.httpClient.get(`/basedatos-api/getIdPedido`);
   }
   //Servicio mostrar un pedido
   getPedido(id: string) {
-    return this.httpClient.get(`http://localhost:3000/pedido?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/pedido?id=${id}`);
   }
   //ventas
   getsales() {
-    return this.httpClient.get('http://localhost:3000/ventas');
+    return this.httpClient.get('/basedatos-api/ventas');
   }
   getventas(id: string) {
-    return this.httpClient.get(`http://localhost:3000/vent?id=${id}`);
+    return this.httpClient.get(`/basedatos-api/vent?id=${id}`);
   }
 
   deleteventa(id_ventas: any) {
-    return this.httpClient.get(`http://localhost:3000/deleteventa?id=${id_ventas}`);
+    return this.httpClient.get(`/basedatos-api/deleteventa?id=${id_ventas}`);
   }
   ediventa(venta: any, id:any) {
     console.log("veeenta---------->")
     console.log(venta);
     console.log("id peeeedido"+id);
-    return this.httpClient.post(`http://localhost:3000/updateventa?id=${id}`, venta);
+    return this.httpClient.post(`/basedatos-api/updateventa?id=${id}`, venta);
   }
   // AGREGAR VENTA
   addVenta(venta: any, inventario: any) {
 //    console.log(inventario);
-    return this.httpClient.post(`http://localhost:3000/addventa?inv=${inventario}`, venta);
+    return this.httpClient.post(`/basedatos-api/addventa?inv=${inventario}`, venta);
   }
 
   deleteped(producto:any){
     let id1 = producto.id_pedido;
-   return this.httpClient.get(`http://localhost:3000/dped?id_pedido=${id1}`);
+   return this.httpClient.get(`/basedatos-api/dped?id_pedido=${id1}`);
   }
 
   /* deleteVenta(venta:any){
